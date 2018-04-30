2aa8351fe0d9ab55377b142127b1a773f17566c3
Matt <m@kevan.tv>
2018-04-30 14:25:53 +0100

    Add random backgrounds

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,13 @@ $(document).ready(function() {
   	 navigation: true,
   });
 
+var $gradients = ['cy','cm','mc','my','yc','ym'];
+var $gradient = $gradients[Math.floor(Math.random() * $gradients.length)];
+$(".wrapper-1 ").addClass($gradient);
+
+var $patterns = ['p1','p2','p3','p4','p5','p6','p7','p8'];
+var $pattern = $patterns[Math.floor(Math.random() * $patterns.length)];
+$(".wrapper-2 ").addClass($pattern);
 
 var $grid = $('.grid').imagesLoaded( function() {
   // init Masonry after all images have loaded
