02bb0b75309ce2430caa14b6000a6a37b3e7b064
Matt Kevan <m@kevan.tv>
2018-05-02 18:12:40 +0100

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,20 @@
 $(document).ready(function() {
 
-// Owl Carousel
-  $("#work-carousel").owlCarousel({
-  	 items : 1,
-  	 singleItem:true,
-  	 autoPlay: false,
-  	 pagination: true,
-  	 navigation: true,
-  });
+  // Page backgrounds
+  var $gradients = ['cy','cm','mc','my','yc','ym'];
+  var $gradient = $gradients[Math.floor(Math.random() * $gradients.length)];
+  $(".wrapper-1 ").addClass($gradient);
 
-var $gradients = ['cy','cm','mc','my','yc','ym'];
-var $gradient = $gradients[Math.floor(Math.random() * $gradients.length)];
-$(".wrapper-1 ").addClass($gradient);
+  var $patterns = ['p1','p2','p3','p4','p5','p6','p7','p8'];
+  var $pattern = $patterns[Math.floor(Math.random() * $patterns.length)];
+  $(".wrapper-2 ").addClass($pattern);
 
-var $patterns = ['p1','p2','p3','p4','p5','p6','p7','p8'];
-var $pattern = $patterns[Math.floor(Math.random() * $patterns.length)];
-$(".wrapper-2 ").addClass($pattern);
-
-var $grid = $('.grid').imagesLoaded( function() {
-  // init Masonry after all images have loaded
-  $grid.masonry({
-    // options...
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-width' 
-  });
-});
-
-// Main menu
+  // Main menu
   $(".overlay-close").click(function(){
       $(".overlay").fadeOut( "fast");
   });
   $("#show").click(function(){
       $(".overlay").fadeIn("fast");
   });
-
 
 });
