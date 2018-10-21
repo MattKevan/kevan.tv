ad44391fb27b3527dd5663f1eaa028732c97b4b7
Matt Kevan <m@kevan.tv>
2018-10-21 21:36:09 +0100

    replace ga with clicky

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
 
-  // Page backgrounds
-  var $gradients = ['cy','cm','mc','my','yc','ym'];
-  var $gradient = $gradients[Math.floor(Math.random() * $gradients.length)];
-  $(".wrapper-1 ").addClass($gradient);
-
-  var $patterns = ['p1','p2','p3','p5','p7','p8'];
-  var $pattern = $patterns[Math.floor(Math.random() * $patterns.length)];
-  $(".wrapper-2 ").addClass($pattern);
-
   // Main menu
   $(".overlay-close").click(function(){
       $(".overlay").fadeOut( "fast");
@@ -16,7 +7,6 @@ $(document).ready(function() {
   $("#show").click(function(){
       $(".overlay").fadeIn("fast");
   });
-
 
  var containerEl = document.querySelector('.grid');
 
