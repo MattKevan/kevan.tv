702a87e5af3852809c41a89d141e21655501102e
Matt Kevan <matt@MattBook-Pro.local>
2019-06-22 20:36:48 +0100

    Replace colours

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+  // Page backgrounds
+  var $gradients = ['cy','cm','mc','my','yc','ym'];
+  var $gradient = $gradients[Math.floor(Math.random() * $gradients.length)];
+  $(".wrapper-1 ").addClass($gradient);
+
+  var $patterns = ['p1','p2','p5','p7','p8'];
+  var $pattern = $patterns[Math.floor(Math.random() * $patterns.length)];
+  $(".wrapper-2 ").addClass($pattern);
+
+
   // Main menu
   $(".overlay-close").click(function(){
       $(".overlay").fadeOut( "fast");
